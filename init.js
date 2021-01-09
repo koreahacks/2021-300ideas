da6870db7aa96d8320751227da0bf2d4642a1ab4
JunHyoek Bang <bangvkf3@gmail.com>
2021-01-09 10:17:43 +0900

    Start express server and setup babel

---
+++ init.js
@@ -0,0 +1,11 @@
+import app from "./app";
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
