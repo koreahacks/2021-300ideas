21bebbca08faf5e79a2945edacf788125b5eec26
JunHyoek Bang <bangvkf3@gmail.com>
2021-01-09 11:46:12 +0900

    Connect to mongo db

---
+++ db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("Connected to DB");
+});
+db.on("error", error => {
+  console.log(`Error on DB Connection:${error}`);
+});
